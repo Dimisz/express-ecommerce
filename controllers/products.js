fa75a2b854d6cb07def2f8ca63b64c8e68db1881
Vladimir Solovyov <leilyngale@icloud.com>
2022-10-05 18:40:40 +0300

    Refactored functionality to use MVC pattern

--- controllers/products.js
+++ controllers/products.js
@@ -16,3 +16,14 @@ exports.getAddProduct = (req, res, next) => {
     products.push({ title: req.body.title });
     res.redirect('/');
   };
+
+  exports.getProducts = (req, res, next) => {
+    res.render('shop', {
+      prods: products, 
+      pageTitle: "Shop", 
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  };
